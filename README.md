7d45d1d08adddc4ca9f625742d58ea41ec2db385
percydev11 <percydeveloper@gmail.com>
2021-07-13 17:16:26 -0500

    Begining the implement the formulas in Js

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # javascript-practical-course
+
 Javascript practical course at Platzi
+
+...
+
+### Workshop #1 - Geometric figures
+
+- Step one: define the  formulas
+- Step two: implement the formulas in JavaScript
+- Step three: create functions
+- Step four: integrate JavaScript with HTML
+
---
+++ figures.js
@@ -0,0 +1,42 @@
+//Square code
+console.group("Squares");
+
+//Sides
+const squareSide = 5;
+console.log("The sides of square measure " + squareSide + "cm");
+
+//Perimeter
+const squarePerimeter = squareSide * 4;
+console.log("The  perimeter of square measure " + squarePerimeter + "cm");
+
+//Area
+const squareArea = squareSide * squareSide;
+console.log("The area of square measure " + squareArea + "cm^2");
+console.groupEnd();
+
+
+//Triangle code
+console.group("Triangles");
+
+//Sides
+const triangleSide1 = 6;
+const triangleSide2 = 6;
+const triangleBase = 4;
+console.log(
+  "The sides of triangle measure: " +
+    triangleSide1 +
+    "cm, " +
+    triangleSide2 +
+    "cm, " +
+    triangleBase +
+    "cm"
+);
+
+
+//Perimeter
+
+console.groupEnd();
+
+
+//Circles code
+console.group("Circle");
