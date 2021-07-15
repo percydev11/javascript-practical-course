c61919aab2f176acbac589b1acac6cdac68d2dd9
percydev11 <percydeveloper@gmail.com>
2021-07-15 13:37:50 -0500

    Calculus the other figures

--- figures.js
+++ figures.js
@@ -2,12 +2,28 @@
 console.group("Squares");
 
 //Perimeter
-//Arrow function
 const squarePerimeter = (side) => side * 4;
 
 //Area
-//Arrow function
 const squareArea = (side) => side * side;
+
+//Interaction with HTML
+function calculatePerimeterSquare() {
+  const input = document.getElementById("InputSquare");
+
+  //Get value of element
+  const value = input.value;
+  const perimeter = squarePerimeter(value);
+  alert("The perimeter the square is = " + perimeter);
+}
+
+function calculateAreaSquare() {
+  const input = document.getElementById("InputSquare");
+  const value = input.value;
+  const area = squareArea(value);
+  alert("The area the squeare is = " + area);
+
+}
 
 console.groupEnd();
 
@@ -15,12 +31,31 @@ console.groupEnd();
 console.group("Triangles");
 
 //Perimeter
-//Arrow function
-const trianglePerimeter = (side1, side, base) => side1 + side + base;
+const trianglePerimeter = (side1, side2, base) => side1 + side2 + base;
 
 //Area
-//Arrow function
 const triangleArea = (base, height) => (base * height) / 2;
+
+function calculatePerimeterTriangle() {
+  const inputSide1 = document.getElementById("InputSide1");
+  const valueSide1 = Number(inputSide1.value);
+  const inputSide2 = document.getElementById("InputSide2");
+  const valueSide2 = Number(inputSide2.value);
+  const inputBase = document.getElementById("InputBase");
+  const valueBase = Number(inputBase.value);
+  const perimeter =  trianglePerimeter(valueSide1, valueSide2, valueBase);
+  alert("The perimeter of triangle is = " + perimeter);
+  
+}
+
+function calculateAreaTriangle() {
+  const inputHeight = document.getElementById("InputHeight");
+  const valueHeight =  Number(inputHeight.value);
+  const inputBase = document.getElementById("InputBase");
+  const valueBase = Number(inputBase.value);
+  const area = triangleArea(valueHeight, valueBase);
+  alert("The area the triangle is = " + area);
+}
 
 console.groupEnd();
 
@@ -28,7 +63,6 @@ console.groupEnd();
 console.group("Circle");
 
 //Diameter
-//Arrow function
 const circleDiameter = (radius) => radius *  2;
 
 //PI
@@ -36,30 +70,22 @@ const PI = Math.PI;
 console.log("PI is = " + PI);
 
 //Circumference
-//Arrow function
 const circlePerimeter = (radius) => circleDiameter(radius) * PI;
 
 //Area
-//Arrow function
 const circleArea = (radius) => PI * radius ** 2;
+
+
+function calculatePerimeterCircle() {
+
+
+}
+
+function calculateAreaCircle() {
+
+
+}
 
 console.groupEnd();
 
-//Interaction with HTML
-function calculatePerimeterSquare() {
-  const input = document.getElementById("inputSquare");
-  const value = input.value;
 
-  const perimeter = squarePerimeter(value);
-  alert(perimeter);
-}
-
-function calculateAreaSquare() {
-
-  const input = document.getElementById("inputSquare");
-  const value = input.value;
-
-  const area = squareArea(value);
-  alert(area);
-
-}
