786625c59822bd2abefd8f63d0d309a3f9c276be
percydev11 <percydeveloper@gmail.com>
2021-07-22 21:48:18 -0500

    Workshop 2 - Prices and discounts js and css ok

--- README.md
+++ README.md
@@ -6,8 +6,15 @@ Javascript practical course at Platzi
 
 ### Workshop #1 - Geometric figures
 
-- Step one: define the  formulas
-- Step two: implement the formulas in JavaScript
-- Step three: create functions
-- Step four: integrate JavaScript with HTML
+- Step one: define the  formulas.
+- Step two: implement the formulas in JavaScript.
+- Step three: create functions.
+- Step four: integrate JavaScript with HTML.
 
+
+### Workshop #2 - Prices and Discounts
+
+- Step one: define the  formulas.
+- Step two: implement the formulas in JavaScript.
+- Step three: create functions.
+- Step four: interact with  HTML from JavaScript.
---
+++ prices-discounts/discounts.js
@@ -0,0 +1,45 @@
+const originalPrice = 120;
+const percentDiscount = 18;
+
+function calculatePriceWithDiscount(originalPrice, percentDiscount) {
+  const percentPriceWithDiscount = 100 - percentDiscount;
+
+  const priceWithDiscount = (originalPrice * percentPriceWithDiscount) / 100;
+
+  return priceWithDiscount;
+}
+
+//Array of objects with name and discount value of the coupons
+const coupons = [
+  { name: "Fonta_Gurt", discount: 15 },
+  { name: "Alpine", discount: 30 },
+  { name: "Parmalat", discount: 25 },
+];
+
+function priceWithDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputCoupon = document.getElementById("InputCoupon");
+  const couponValue = inputCoupon.value;
+
+  //Validate the user's coupon against the array
+  const isCouponValid = function (coupon) {
+    return coupon.name === couponValue;
+  };
+
+  //Variable with name and discount value
+  const userCoupon = coupons.find(isCouponValid);
+
+  if (!userCoupon) {
+    alert("The coupon " + couponValue + " is invalid");
+  } else {
+    const discount = userCoupon.discount;
+    const priceMinusDiscount = calculatePriceWithDiscount(priceValue, discount);
+    const resultPrice = document.getElementById("ResultPrice");
+    //Write in HTML with innerText
+    resultPrice.innerText = "The price with discount is = $" + priceMinusDiscount;
+  }
+}
+
+
