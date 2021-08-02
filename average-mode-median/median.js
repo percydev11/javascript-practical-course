d891634e9628538d5d71b2c02a2f02eff169ac5f
percydev11 <percydeveloper@gmail.com>
2021-08-02 16:05:58 -0500

    Median interaction between HTML and JS ok

--- average-mode-median/median.js
+++ average-mode-median/median.js
@@ -5,12 +5,19 @@ function calculateMedian(list) {
 
   const middleList = parseInt(orderedList.length / 2);
 
+  let median;
+  function isPair(number) {
+    if (number % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   if (isPair(orderedList.length)) {
     const elementOne = orderedList[middleList - 1];
     const elementTwo = orderedList[middleList];
-
     const averageElementsOneTwo = calculateAverage([elementOne, elementTwo]);
-
     median = averageElementsOneTwo;
   } else {
     median = orderedList[middleList];
@@ -19,19 +26,22 @@ function calculateMedian(list) {
   return median;
 }
 
-let median;
-function isPair(number) {
-  if (number % 2 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 function calculateAverage(list) {
   const sumList = list.reduce(function (acumulatedValue = 0, newValue) {
     return acumulatedValue + newValue;
   });
   const averageList = sumList / list.length;
   return averageList;
+}
+
+function median() {
+  const inputNumbers = document.getElementById("InputNumbers");
+  const numbersValue = inputNumbers.value;
+  // Convert the string to an array
+  const arrayValue = Array.from(numbersValue.split(","), Number);
+  const resultMedian = calculateMedian(arrayValue);
+  const result = document.getElementById("ResultMedian");
+  result.innerText = "The median is " + resultMedian;
+    
+  
 }
